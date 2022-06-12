710a8dfbfdc3e27aee12f8e4ecd34f9f2c03d5c6
panutat-p <pp.panutat@gmail.com>
2022-06-13 00:15:41 +0700

    run-es-module, run script

---
+++ src/run-es-module/run.ts
@@ -0,0 +1,29 @@
+import {
+  InvocationType,
+  InvokeCommand,
+  LambdaClient,
+  LogType,
+} from '@aws-sdk/client-lambda';
+import { decode } from 'base-64';
+
+const client = new LambdaClient({ region: 'ap-southeast-1' });
+
+const command = new InvokeCommand({
+  FunctionName: 'run-es-module',
+  InvocationType: InvocationType.RequestResponse,
+  LogType: LogType.Tail,
+});
+
+(async () => {
+  try {
+    const data = await client.send(command);
+    console.log(data.StatusCode);
+    if (data.LogResult != null) {
+      console.log(`\n${decode(data.LogResult)}`);
+    }
+    console.log(data.Payload);
+    console.log(new TextDecoder('utf-8').decode(data.Payload));
+  } catch (e) {
+    console.log(e);
+  }
+})();
