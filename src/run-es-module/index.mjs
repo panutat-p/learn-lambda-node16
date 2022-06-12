7c9a71771dc563a1f5b3505917363162703c68e5
panutat-p <pp.panutat@gmail.com>
2022-06-13 00:15:20 +0700

    run-es-module, function code

---
+++ src/run-es-module/index.mjs
@@ -0,0 +1,15 @@
+console.log("init 🟩");
+
+(async () => {
+  await new Promise(r => setTimeout(r, 2000));
+  console.log("configured 🟧");
+})();
+
+export async function handler() {
+  await new Promise(r => setTimeout(r, 2000));
+  const response = {
+    statusCode: 200,
+    body: 'Hello 🟦',
+  };
+  return response;
+}
